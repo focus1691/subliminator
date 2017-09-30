89df3923b7cbd8d6f89b83a8df17e67a993b7998
Joshua <mrjoshuaali@yahoo.co.uk>
2017-09-30 21:27:14 +0100

    Fixed messages writing over each other. Added white background to message & font.

--- Mind Booster/src/com/psychotechnology/GUI/SubliminalMessage.java
+++ Mind Booster/src/com/psychotechnology/GUI/SubliminalMessage.java
@@ -7,6 +7,7 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
@@ -24,33 +25,39 @@ public class SubliminalMessage extends JPanel {
 		setOpaque(false);
 		setPreferredSize(new Dimension(600, 300));
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		int x, y;
 		
-	    FontMetrics metrics = g2d.getFontMetrics(new Font("Courier New", Font.BOLD, 24));
-	    x = this.getX() + (getWidth() - metrics.stringWidth(message)) / 2;
-	    g2d.setFont(new Font("TimesRoman", Font.BOLD, 24));
-	    g2d.setColor(Color.BLACK);
-	    
-	    //g.setFont(new Font("TimesRoman", Font.BOLD, 24));
-	   
-	    g2d.drawString(message, x, 40);
-
+		Graphics2D g2d = (Graphics2D) g;
+		FontMetrics metrics = g2d.getFontMetrics(new Font("Courier New", Font.BOLD, 24));
+		Rectangle2D rect = metrics.getStringBounds(message, g2d);
+		int x = this.getX() + (getWidth() - metrics.stringWidth(message)) / 2;
+		int y;
+		
+		g2d.clearRect(0, 0, getWidth(), getHeight());
+		
+		g2d.setFont(new Font("TimesRoman", Font.BOLD, 24));
+		g2d.setColor(Color.WHITE);
+        g2d.fillRect(x,
+                40 - metrics.getAscent(),
+                (int) rect.getWidth(),
+                (int) rect.getHeight());
+        g2d.setColor(Color.BLACK);
+		g2d.drawString(message, x, 40);
+		
 		if (img != null) {
-		    x = (this.getWidth() - img.getWidth(null)) / 2;
-		    y = (this.getHeight() - img.getHeight(null)) / 2;
-		    g2d.drawImage(img, x, y, this);
+			x = (this.getWidth() - img.getWidth(null)) / 2;
+			y = (this.getHeight() - img.getHeight(null)) / 2;
+			g2d.drawImage(img, x, y, this);
 		}
 	}
-
+	
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
+	
 	public void setImage(Image image) {
 		this.img = image;
 	}
