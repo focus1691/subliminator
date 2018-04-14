65704f025dddee46fa04a0fa52ce9e45c2348b21
Joshua <mrjoshuaali@yahoo.co.uk>
2018-04-14 13:36:58 +0100

    restore new files

--- Mind Booster/src/constants/ScreenPosition.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public enum ScreenPosition {
-	TOPLEFT, TOPRIGHT, CENTER, BOTLEFT, BOTRIGHT
-}
--- Mind Booster/src/gui/message/MessageSelectionListener.java
+++
@@ -1,8 +0,0 @@
-package gui.message;
-
-import java.util.EventListener;
-
-public interface MessageSelectionListener extends EventListener {
-
-	public void messageSelectionEventOccurred(MessageSelectionEvent e);
-}
